a270a31e3154165571c55f43e524786daba361d1
rialbeg <gsamascarenhas@gmail.com>
2021-11-16 12:32:39 -0300

    iniciando as rotas

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+    print("<pre>".print_r($_GET,true)."</pre>");
+    //testa a variável url que veio lá do htaccess
+	// if (isset($_GET['url'])) //se estiver preenchida, pega o valor
+    // {
+    //     $url =  strtoupper($_GET['url']);
+    //     switch ($url){
+    //         case "HOME":
+    //             echo "estamos na home";
+    //             break;
+    //         default:
+    //             echo "estamos no default";
+    //             break;
+    //     }
+    // }
+    // else                     //senão, vai para uma página padrão, neste caso a home do site
+    //     $url = '404.php';
+?>
+
